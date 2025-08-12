867f2863f84e378b8857b9dfa38ba694736aa75a
tanmoykdas <tnmy468@gmail.com>
2025-08-12 15:45:54 +0600

    added new line

--- input_output.asm
+++ input_output.asm
@@ -9,7 +9,15 @@ main proc
     
     mov ah,2
     mov dl,bl
-    int 21h 
+    int 21h
+    
+    mov ah,2
+    mov dl,10
+    int 21h
+    
+    mov ah,2
+    mov dl, 13
+    int 21h  
     
     mov ah,1
     int 21h
