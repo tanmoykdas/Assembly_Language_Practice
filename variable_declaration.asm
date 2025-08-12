3197a3228e0d5ee5746179093f43f6959a564703
tanmoykdas <tnmy468@gmail.com>
2025-08-13 01:06:24 +0600

    modifid again

--- variable_declaration.asm
+++ variable_declaration.asm
@@ -6,38 +6,30 @@ b db ?
 .code
 
 main proc
-    mov ax, @data
-    mov ds, ax
-    
-    mov ah,1
-    int 21h
-    mov b,al
-    mov bl, al
-    
-    mov ah,2
-    mov dl, 10
-    int 21h
-    
-    mov ah,2
-    mov dl,13
-    int 21h
-    
-    mov ah,2
-    mov dl, a
-    add dl, 48
-    int 21h
-    
-    mov ah,2
-    mov dl, 10
-    int 21h
-    
-    mov ah,2
-    mov dl,13
-    int 21h 
-    
-    mov ah,2
-    mov dl, bl
-    int 21h
+     mov ax,@data
+     mov ds,ax
+     
+     mov ah,1
+     int 21h
+     mov b,al
+     
+     mov ah,2
+     mov dl, b
+     int 21h
+     
+     
+     mov ah,2
+     mov dl,10
+     int 21h
+     
+     mov ah,2
+     mov dl,13
+     int 21h
+     
+     mov ah,2
+     add a,48
+     mov dl,a
+     int 21h
     
     exit:
     mov ah,4ch
