c21590c6b471158311001810ef2d4319cbfac550
tanmoykdas <tnmy468@gmail.com>
2025-08-13 19:12:01 +0600

    user friendly input updated

--- String_printing.asm
+++ String_printing.asm
@@ -1,35 +1,16 @@
 .model small
 .stack 256
 .data
-a db 5
-b db ?
+a db 'My name is Tanmoy $'
 .code
 
 main proc
-     mov ax,@data
-     mov ds,ax
-     
-     mov ah,1
-     int 21h
-     mov b,al
-     
-     mov ah,2
-     mov dl, b
-     int 21h
-     
-     
-     mov ah,2
-     mov dl,10
-     int 21h
-     
-     mov ah,2
-     mov dl,13
-     int 21h
-     
-     mov ah,2
-     add a,48
-     mov dl,a
-     int 21h
+    mov ax,@data
+    mov ds,ax
+    
+    mov ah,9
+    lea dx,a   
+    int 21h
     
     exit:
     mov ah,4ch
---
+++ user_friendly_input_output.asm
@@ -0,0 +1,39 @@
+.model small
+.stack 256
+.data
+a db 'Enter a Character: $'
+b db 'Your Character is: $'
+
+.code
+main proc
+    mov ax,@data
+    mov ds,ax
+    
+    mov ah,9
+    lea dx,a
+    int 21h
+    
+    mov ah,1  
+    int 21h
+    mov bl,al 
+    
+    mov ah,2
+    mov dl,10
+    int 21h
+    
+    mov ah,2
+    mov dl,13
+    int 21h   
+    
+    mov ah,9
+    lea dx,b
+    int 21h
+    
+    mov ah,2
+    mov dl,bl
+    int 21h 
+    
+    mov ah,4ch
+    int 21h
+main endp
+end main
