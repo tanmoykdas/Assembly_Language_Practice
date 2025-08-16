80736bd621794e86bb9407e9f34258cbccc44166
tanmoykdas <tnmy468@gmail.com>
2025-08-17 01:32:58 +0600

    divison of two static number

---
+++ division.asm
@@ -0,0 +1,31 @@
+.model samll
+.stack 256
+.code
+
+main proc  
+    ;divison of two number
+    mov al,4
+    mov bl,2 
+    
+    div bl
+    
+    mov cl,al
+    mov ch,ah 
+    
+    mov ah,2
+    mov dl,cl
+    add dl,48
+    int 21h
+    
+    mov ah,2
+    mov dl,ch 
+    add dl,48
+    int 21h
+    
+    exit:
+    mov ah,4ch
+    int 21h
+    main endp
+end main
+    
+    
